578d6b8cd875718f322cbcc5c84252d860946a20
ROHIT SRIVASTAVA <57180337+rohitsrivastava9@users.noreply.github.com>
2020-04-28 16:16:30 +0530

    Initial Version
    A Restful service built on SpringBoot and Maven, written in Java.

---
+++ SpringBootRestful/src/test/java/com/rohit/poc/SpringBootRestful/AppTest.java
@@ -0,0 +1,41 @@
+package com.rohit.poc.SpringBootRestful;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;  
+
+
+/**
+ * Unit test for simple App.
+ */
+@SpringBootTest  
+public class AppTest 
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+		return null;
+       
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
